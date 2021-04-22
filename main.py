ed49ed7268db0b1d8bef62a48b6c63d5f7b8b2da
bigkjp97 <kunzhoupeng@gmail.com>
2021-04-22 12:31:37 +0800

    v1.5

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+import time
 import threading
 
 from prometheus_client import start_http_server
@@ -17,17 +19,28 @@ def main():
         config = yaml.load(conf, Loader=yaml.FullLoader)
     # print(config)
     pushHost = config['server']['host']
-    pushPort = int(config['server']['port'])
+    pushPort = config['server']['port']
     # 'config': [{'job': '', 'file': '', 'keyword': ['', ''], 'label': ['', '']}]
     cases = config['config']
     hostName = socket.gethostname()
-    start_http_server(pushPort, pushHost)
+    print("[MOLOGOV][INFO] " + time.strftime("%Y-%m-%d %H:%M:%S",
+                                       time.localtime()) + " Start - " + pushHost + ":" + pushPort)
+    try:
+        start_http_server(int(pushPort), pushHost)
+    except:
+        print("[MOLOGOV][ERROR] " + time.strftime("%Y-%m-%d %H:%M:%S",
+                                                  time.localtime()) + " Bad server " + pushHost + ":" + pushPort)
+        sys.exit(1)
+
+    print("[MOLOGOV][INFO] " + time.strftime("%Y-%m-%d %H:%M:%S",
+                                       time.localtime()) + " Start successfully " + pushHost + ":" + pushPort)
+
     for case in cases:
         file = case['file']
         keywords = case['keywords']
         label = case['label']
         matric = case['matric']
-        print(case)
+        print("[MOLOGOV][INFO] " + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + " Monitoring " + file)
         # use multiple threads
         t = threading.Thread(target=tail.Tail(file, keywords, label, matric, hostName).tail_keywords)
         t.start()
--- utils/tail.py
+++ utils/tail.py
@@ -16,9 +16,6 @@ class Tail():
 
     # listen when keyword appears in log
     def tail_keywords(self):
-        # register = CollectorRegistry()
-        # count = Gauge('words_count', 'Count keywords from log', ['keyword'],
-        #               registry=register)
         # move cursor after reading file
         file = self._file
         # print(file)
@@ -26,22 +23,34 @@ class Tail():
         arrKeywords = self._keywords
         # labels match keywords
         arrLabel = self._labelName
-        with open(file) as log:
-            while True:
-                # if log was cleaned, read from start
-                if os.path.getsize(file) == 0:
-                    log.seek(0, 0)
-                    # off this loop
-                    # continue
-                where = log.tell()
-                line = log.readline()
-                # print(line)
-                if not line:
-                    time.sleep(1)
-                    # remain point last char
-                    log.seek(where)
-                else:
-                    for keyword in arrKeywords:
-                        if keyword in line:
-                            self._count.labels(keyword=arrLabel[arrKeywords.index(keyword)],
-                                               instance=self._hostName).inc()
+        maxTry = 0
+        while maxTry < 5:
+            try:
+                with open(file) as log:
+                    maxTry = 0
+                    while True:
+                        # if log was cleaned, read from start
+                        if os.path.getsize(file) == 0:
+                            log.seek(0, 0)
+                            # off this loop
+                            # continue
+                        where = log.tell()
+                        line = log.readline()
+                        # print(line)
+                        if not line:
+                            time.sleep(1)
+                            # remain point last char
+                            log.seek(where)
+                        else:
+                            for keyword in arrKeywords:
+                                if keyword in line:
+                                    self._count.labels(keyword=arrLabel[arrKeywords.index(keyword)],
+                                                       instance=self._hostName).inc()
+            except FileNotFoundError:
+                maxTry += 1
+                print("[MOLOGOV][ERROR] " + time.strftime("%Y-%m-%d %H:%M:%S",
+                                                          time.localtime()) + " File " + file + " not found")
+                time.sleep(600)
+
+        print("[MOLOGOV][INFO] " + time.strftime("%Y-%m-%d %H:%M:%S",
+                                                 time.localtime()) + " Stop monitoring " + file)
